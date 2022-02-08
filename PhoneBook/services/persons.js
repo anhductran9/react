f207c4f4311dd9cd3ed0baef46aa588d26387ec6
TRAN ANH DUC <70715779+anhductran9@users.noreply.github.com>
2022-02-08 22:06:51 +0700

    Update persons.js

--- PhoneBook/services/persons.js
+++ PhoneBook/services/persons.js
@@ -33,4 +33,9 @@ const deleteData = (id) => {
     return request.then(response => response.data)
 }
 
-export default {getAll, create, update, deleteData}
+const replaceData = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default {getAll, create, update, deleteData, replaceData}
